a074658868c8e4eefe92dd4e6f00a39d8b094e5f
Badrmoh <engbadr@outlook.com>
2026-08-13 12:20:55 +0200

    feat: enable ssh-agent

--- nix/hosts/work.nix
+++ nix/hosts/work.nix
@@ -6,6 +6,14 @@
     ../packages/editor.nix
     ../programs
   ];
+  services.ssh-agent.enable = true;
+  # programs.ssh = {
+  #   enable = true;
+  #   enableDefaultConfig = false;
+  #   settings."*" = {
+  #     AddKeysToAgent = "yes";
+  #   };
+  # };
   home.packages = with pkgs; [openbao consul-template glab k9s helm rclone awscli kcl];
   home.username = "badr";
   home.homeDirectory = "/home/badr";
